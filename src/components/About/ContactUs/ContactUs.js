aa13d826579d8e32d7c7127e10392715d712bd5d
Mehedi390 <mh0816150@gmail.com>
2022-11-15 05:29:36 -0800

    asdf

--- src/components/About/ContactUs/ContactUs.js
+++ src/components/About/ContactUs/ContactUs.js
@@ -2,70 +2,71 @@ import React from "react";
 import MainLayout from "../../ShareComponents/MainLayout/MainLayout";
 import mehedi from "../../../images/profile.png";
 import naimur from "../../../images/naimur.jpg";
+import { Grid } from "@mui/material";
 const ContactUs = () => {
   return (
     <MainLayout>
       <h1>contact us</h1>
-      <div className="flex">
-        <div className=" ml-28 p-4">
-          <section className="absolute top-1/2 left-3/2 h-96 w-80 -translate-x-1/2 -translate-y-1/2 rotate-0     transform rounded-2xl bg-gray-400"></section>
-
-          <section className="absolute top-1/2  h-96 w-80 -translate-x-1/2 -translate-y-1/2 rotate-3 transform space-y-6 rounded-2xl bg-green-100 p-6 duration-300 ">
-            <div className="flex justify-between">
-              <div className="">
-                <h1 className="text-xl font-bold text-gray-700">
-                  {" "}
-                  Mehedi Hasan
-                </h1>
+      <Grid container spacing={2}>
+        <Grid item lg={6} sm={12}>
+          <div className="   p-4">
+            <section className="    h-96 w-80  hover:rotate-3 hover:shadow-2xl shadow-lg transform space-y-6 rounded-2xl bg-green-100 p-6 duration-300 ">
+              <div className="flex justify-between">
+                <div className="">
+                  <h1 className="text-xl font-bold text-gray-700">
+                    {" "}
+                    Mehedi Hasan
+                  </h1>
+                </div>
+                <div className=" w-28  rounded-full   ">
+                  <img className="rounded-full -mt-10" src={mehedi} alt="" />
+                </div>
               </div>
-              <div className=" w-28  rounded-full   ">
-                <img className="rounded-full -mt-10" src={mehedi} alt="" />
+
+              <header className="text-center text-xl font-extrabold text-gray-600">
+                Frontend Web developer
+              </header>
+
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptates harum voluptas incidunt delectus veniam deserunt
+                  repudiandae quas, quis placeat esse.
+                </p>
               </div>
-            </div>
-
-            <header className="text-center text-xl font-extrabold text-gray-600">
-              Frontend Web developer
-            </header>
-
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates harum voluptas incidunt delectus veniam deserunt
-                repudiandae quas, quis placeat esse.
-              </p>
-            </div>
-          </section>
-        </div>
-        <div className=" ml-28 p-4">
-          <section className="absolute top-1/2 left-2/3 h-96 w-80 -translate-x-1/2 -translate-y-1/2 rotate-0     transform rounded-2xl bg-gray-400"></section>
-
-          <section className="absolute top-1/2  left-2/3 h-96 w-80 -translate-x-1/2 -translate-y-1/2 rotate-3 transform space-y-6 rounded-2xl bg-pink-100 p-6 duration-300 ">
-            <div className="flex justify-between">
-              <div className="">
-                <h1 className="text-xl font-bold text-gray-700">
-                  {" "}
-                  Naimur Rahaman
-                </h1>
+            </section>
+          </div>
+        </Grid>{" "}
+        <Grid item lg={6} sm={12}>
+          <div className="   p-4">
+            <section className="  hover:rotate-3 hover:shadow-2xl shadow-lg      h-96 w-80    transform space-y-6 rounded-2xl bg-pink-100 p-6 duration-300 ">
+              <div className="flex justify-between">
+                <div className="">
+                  <h1 className="text-xl font-bold text-gray-700">
+                    {" "}
+                    Naimur Rahaman
+                  </h1>
+                </div>
+                <div className=" w-28  rounded-full   ">
+                  <img className="rounded-full -mt-10" src={naimur} alt="" />
+                </div>
               </div>
-              <div className=" w-28  rounded-full   ">
-                <img className="rounded-full -mt-10" src={naimur} alt="" />
+
+              <header className="text-center text-xl font-extrabold text-gray-600">
+                Frontend Web developer
+              </header>
+
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptates harum voluptas incidunt delectus veniam deserunt
+                  repudiandae quas, quis placeat esse.
+                </p>
               </div>
-            </div>
-
-            <header className="text-center text-xl font-extrabold text-gray-600">
-              Frontend Web developer
-            </header>
-
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates harum voluptas incidunt delectus veniam deserunt
-                repudiandae quas, quis placeat esse.
-              </p>
-            </div>
-          </section>
-        </div>
-      </div>
+            </section>
+          </div>
+        </Grid>
+      </Grid>
     </MainLayout>
   );
 };

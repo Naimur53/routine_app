6bcb6eea29c75e1afc1ae61b3b82bc463ec166b8
Mehedi390 <mh0816150@gmail.com>
2022-11-16 00:17:37 -0800

    asdf

--- src/components/MyNote/HomeNoteShow.js
+++ src/components/MyNote/HomeNoteShow.js
@@ -1,21 +1,37 @@
+import { Button, Grid } from "@mui/material";
 import React, { useState } from "react";
 import { getDataFromLocalDb } from "../../utilities/localDb";
-
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 const HomeNoteShow = ({ list }) => {
-
   return (
-    <div className="overflow-y-auto">
+    <Grid container spacing={4}>
       {list?.slice(0, 3).map((singleCacheItem, i) => (
         <>
-          <div className="bg-light-purple p-3  rounded-xl mb-2 text-justify break-words">
-            <div>
-              <p className="font-bold  ">{singleCacheItem.title}</p>
-              <p>{singleCacheItem.note}</p>
+          <Grid item md={12} lg={12}>
+            <div class="bg-white border-t-4 border-t-green-300  p-5   rounded-tl-xl rounded-br-xl shadow-md w-[320px]   min-h-[80px]">
+              <div className="flex justify-between">
+                {" "}
+                <h1 title="title" class="text-xl font-bold">
+                  {singleCacheItem.title}
+                </h1>
+              </div>
+
+              <div class="mb-3">
+                <div class="bg-gray-400 w-24 h-1 rounded-lg mt-2   overflow-hidden">
+                  <div class="bg-gray-100 w-full h-full rounded-lg shadow-md"></div>
+                </div>
+              </div>
+
+              <h2 title="note" class="tracking-wide">
+                {singleCacheItem.note}
+              </h2>
+              <div className="mt-2"></div>
             </div>
-          </div>
+          </Grid>
         </>
       ))}
-    </div>
+    </Grid>
   );
 };
 

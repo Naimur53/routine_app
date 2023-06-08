58b9ad173c49214b52f9df0df8645428e4ec5ea6
naimur53 <naimurrhman53@gmail.com>
2023-06-09 00:36:09 +0600

    eon

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from './ManageState/Store/store';
 import { Workbox } from 'workbox-window';
 
 if ('serviceWorker' in navigator) {
-  const wb = new Workbox('/service-worker.js');
+  const wb = new Workbox('./service-worker.js');
 
   wb.register()
     .then(registration => {
@@ -25,7 +25,6 @@ root.render(
 
 
     <App />
-
   </Provider>
 );
 
